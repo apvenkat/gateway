997532b943ca4cb31eb8c12bf14260baa528f500
apvenkat <venkat.subramanian94@gmail.com>
2020-03-18 22:14:25 +0800

    changed ssl tunnel

--- config/default.js
+++ config/default.js
@@ -6,10 +6,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-'use strict';
+"use strict";
 
-const os = require('os');
-const path = require('path');
+const os = require("os");
+const path = require("path");
 const home = os.homedir();
 
 module.exports = {
@@ -21,19 +21,17 @@ module.exports = {
   ports: {
     https: 4443,
     http: 8080,
-    ipc: 9500,
+    ipc: 9500
   },
   // Whether the gateway is behind port forwarding and should use simplified
   // port-free urls
   behindForwarding: true,
   addonManager: {
-    listUrls: [
-      'https://api.mozilla-iot.org:8443/addons',
-    ],
-    testAddons: false,
+    listUrls: ["https://api.mozilla-iot.org:8443/addons"],
+    testAddons: false
   },
   database: {
-    removeBeforeOpen: false,
+    removeBeforeOpen: false
   },
   settings: {
     defaults: {
@@ -41,38 +39,38 @@ module.exports = {
         localAccess: true,
         mozillaTunnelService: true,
         localControl: {
-          mdnsServiceType: 'http',
-          mdnsServiceName: 'Mozilla WebThings Gateway',
-          mdnsServiceDomain: os.hostname().split('.')[0],
+          mdnsServiceType: "http",
+          mdnsServiceName: "Mozilla WebThings Gateway",
+          mdnsServiceDomain: os.hostname().split(".")[0],
 
           mdnsTxt: {
-            desc: 'Web of Things Gateway',
-            protocol: 'http, https, Web Sockets',
-            power: '6 watts',
-          },
-        },
-      },
-    },
+            desc: "Web of Things Gateway",
+            protocol: "http, https, Web Sockets",
+            power: "6 watts"
+          }
+        }
+      }
+    }
   },
   authentication: {
-    defaultUser: null,
+    defaultUser: null
   },
   ssltunnel: {
     enabled: true,
-    registration_endpoint: 'https://api.mozilla-iot.org:8443',
-    domain: 'mozilla-iot.org',
-    pagekite_cmd: path.normalize(path.join(process.cwd(), 'pagekite.py')),
+    registration_endpoint: "https://api.techbeast.org:8443",
+    domain: "techbeast.org",
+    pagekite_cmd: path.normalize(path.join(process.cwd(), "pagekite.py")),
     port: 443,
-    certemail: 'certificate@mozilla-iot.org',
+    certemail: "hello@riyasaalabs.com"
   },
   bcryptRounds: 2,
-  updateUrl: 'https://api.mozilla-iot.org:8443/releases',
+  updateUrl: "https://api.mozilla-iot.org:8443/releases",
   wifi: {
     ap: {
-      ipaddr: '192.168.2.1',
-      ssid_base: 'WebThings Gateway',
-    },
+      ipaddr: "192.168.2.1",
+      ssid_base: "WebThings Gateway"
+    }
   },
   oauthPostToken: false,
-  oauthTestClients: false,
+  oauthTestClients: false
 };
